4976a291a1631a42603ea2126c2aa3d0f4ee2556
gaeun113 <gaeun113@naver.com>
2020-07-02 20:37:33 +0900

    message

---
+++ test.py
@@ -0,0 +1,9 @@
+x = input("첫 번쨰 정수 : ")
+y = input("두 번쨰 정수 : ")
+sum = int(x) + int(y)
+print("합은 ", sum)
+
+name = input("이름을 입력해주세요.")
+print(name + "님 반갑습니다.")
+age = input("나이를 입력해주세요.")
+print("10년 후면 ", int(age)+10, "살이 되시는군요!")
